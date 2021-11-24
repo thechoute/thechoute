d44f7a2660f90cd095f8c0448e596a843be76ba9
thechoute <82709682+thechoute@users.noreply.github.com>
2021-11-24 17:57:27 -0400

    Update menu.js

--- libreria/menu.js
+++ libreria/menu.js
@@ -1,3 +1,24 @@
 const allmenu = `
 Hola {} este es el menu completo del bot.
+╭
+⎸
+
+☣ USO BAJO SU PROPIA SEGURIDAD ☣
+▸${prefijo}
+▸${prefijo}
+▸${prefijo}
+▸${prefijo}
+▸${prefijo}
+▸${prefijo}
+▸${prefijo}
+▸${prefijo}
+▸${prefijo}
+▸${prefijo}
+▸${prefijo}
+▸${prefijo}
+▸${prefijo}
+▸${prefijo}
+▸${prefijo}
+▸${prefijo}
+▸${prefijo}
 `
